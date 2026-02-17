fdecf57390473c923a4ad8fb15f80501d922dee4
Panna <teri@mwtan.com>
2026-02-17 19:49:58 +0600

    updated mails config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/php/:path*",
+        destination: "http://localhost/freelancing/northernlightstanspa/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
