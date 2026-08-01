a9bdf49abef0baad611e7246b2b3a23b2c211cfd
Md Hasibur Rahman Panna <teri@mwtan.com>
2026-08-01 09:35:48 +0600

    update august speacials

--- src/app/promotions/page.tsx
+++ src/app/promotions/page.tsx
@@ -38,8 +38,8 @@ export default function PromotionsPage() {
             {/* January Specials Card */}
             <div className="relative rounded-lg overflow-hidden shadow-xl mb-12">
               <Image
-                src="/img/promotion/july.png"
-                alt="July Specials"
+                src="/img/promotion/august.png"
+                alt="August Specials"
                 width={800}
                 height={800}
                 className="w-full h-auto"
